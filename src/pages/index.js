9cbd8181e720e373b899fbf9aa1e7f1cd3bced50
Yu Shao, Pang <p.yushao2@gmail.com>
2022-02-17 21:14:10 +0800

    Add resume in homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import styles from './index.module.css';
 import ExperiencesPanel from '../components/ExperiencesPanel';
+import ResumePanel from '../components/ResumePanel';
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
@@ -34,6 +35,7 @@ export default function Home() {
       <HomepageHeader />
       <main>
         <ExperiencesPanel />
+        <ResumePanel />
       </main>
     </Layout>
   );
